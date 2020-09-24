e9f52cbbc613c7364fadcd2a66fbc21344513363
Federico Rossi <tele0x@gmail.com>
2020-09-23 23:28:41 -0500

    Separate init for networking

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,11 @@
 VALID_CNFs="mongodb nrf pcf smf ausf udm udr amf nssf upf uesim"
 
 case "$1" in
-	"all")
+	"init")
+		echo -e "Initialize networking"
 		ansible-playbook -i localhost playbooks/initialize_network.yml
+		;;
+	"all")
 		for nf in $VALID_CNFs; do
 			echo -e "Deploy 5G Core NFs: $VALID_CNFs"
 			ansible-playbook -v -i localhost playbooks/deploy/deploy_$nf.yml
@@ -13,7 +16,6 @@ case "$1" in
 
 	"mongodb" | "nrf" | "pcf" | "smf" | "ausf" | "nssf" | "udm" | "udr" | "amf" | "n3iwf" | "upf" | "uesim")
 		export CNF=$1
-		ansible-playbook -i localhost playbooks/initialize_network.yml
 		echo -e "Deploy 5G Core NF: `echo $1 | tr '[a-z]' '[A-Z]'`"
 		ansible-playbook -i localhost playbooks/deploy/deploy_$CNF.yml
 		;;
